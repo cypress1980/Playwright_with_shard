723da1ef90bc880d7fddc9fe8bb4acf1574a8fc3
Kailash Chander Pathak <you@example.com>
2023-12-19 15:24:37 +0530

    added total 12 tc

--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -52,3 +52,12 @@ test('Test 11 get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+test('Test 12 get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
