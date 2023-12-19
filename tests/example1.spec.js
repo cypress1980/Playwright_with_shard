9f1bfff50020d4d6c3a0bc8dc736b12e5bc9e81b
Kailash Chander Pathak <you@example.com>
2023-12-19 15:10:05 +0530

    added 1 spec 5 test in each .spec

--- tests/example1.spec.js
+++ tests/example1.spec.js
@@ -7,7 +7,6 @@ test('Test 1 has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
-
 test('Test 2 get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
@@ -27,6 +26,15 @@ test('Test 3 get started link', async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
 test('Test 4 get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+test('Test 5 get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
