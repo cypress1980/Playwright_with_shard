3d6d99cd261abc3d075e043fa40083c351cfb590
Kailash Chander Pathak <you@example.com>
2023-12-21 14:48:13 +0530

    added one mor tc

--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -1,13 +1,22 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
-test('Test 6 has title', async ({ page }) => {
+test('Test 7 has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
-test('Test 7 get started link', async ({ page }) => {
+test('Test 8 get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+test('Test 9 get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.
