c79d45762a055d360d681e3b0e599b8a4146e177
Kailash Chander Pathak <you@example.com>
2024-05-02 10:51:05 +0530

    test with 6 shard

--- tests/example1.spec.js
+++ tests/example1.spec.js
@@ -135,8 +135,6 @@ test('Test 15 get started link', async ({ page }) => {
 });
 test('Test 16 get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-  // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
-  // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
