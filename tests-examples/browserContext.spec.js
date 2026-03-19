ecc6634d07d1796e85f8681530a36a7ae2699c7f
KailashPathak <cypress1980@example.com>
2026-03-19 19:51:31 +0530

    Added Flag:true

--- tests-examples/browserContext.spec.js
+++ tests-examples/browserContext.spec.js
@@ -4,7 +4,7 @@ test("Browser Context example", async ({ browser }) => {
   //First Context - Open Page in New Browser
   const firstContext = await browser.newContext();
   const firstPage = await firstContext.newPage();
-  await firstPage.goto("https://automationexercise.com/login");
+  await firstPage.goto('https://automationexercise.com/login', { waitUntil: 'domcontentloaded' });
   await firstPage.locator('input[data-qa="login-email"]').click();
   await firstPage.locator('input[data-qa="login-email"]').fill("ncrmeet1@yopmail.com");
   await firstPage.locator('input[data-qa="login-email"]').press("Tab");
@@ -23,7 +23,7 @@ test("Browser Context example", async ({ browser }) => {
   //Third Context --  Open Page in New Browser
   const thirdContext = await browser.newContext();
   const thirdPage = await thirdContext.newPage();
-  await thirdPage.goto("https://automationexercise.com/login");
+  await thirdPage.goto('https://automationexercise.com/login', { waitUntil: 'domcontentloaded' });
   await thirdPage.locator('input[data-qa="login-email"]').click();
   await thirdPage.locator('input[data-qa="login-email"]').fill("ncrmeet2@yopmail.com");
   await thirdPage.locator('input[data-qa="login-email"]').press("Tab");
@@ -32,14 +32,16 @@ test("Browser Context example", async ({ browser }) => {
   firstPage.bringToFront();
   await firstPage.locator('//a[@href="/products"]').click();
 
-  await firstPage.pause();
-  await secondPage.pause();
+  //await firstPage.pause();
+  //await secondPage.pause();
   await thirdPage.pause();
 
-  /* //Closed Page/ Context
+  //Closed Page Context
     await firstPage.close();
     await secondPage.close();
+    await thirdPage.close();
 
     await firstContext.close();
-    await secondContext.close();*/
+    await secondContext.close();
+    await thirdContext.close();
 });
